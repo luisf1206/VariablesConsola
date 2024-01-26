32a32fe4f4c97428a10cfc0f724b51678ef33f87
LUIS.MARINC <LUIS.MARINC@DIPRO-ARPRO1>
2024-01-26 16:00:36 -0500

    Agregar archivos de proyecto.

---
+++ VariablesConsola/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace VariablesConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero = 5;
+            String cadena = "c#";
+            bool valor = false;
+
+
+            Console.WriteLine("la variable entera tiene: {0}", entero);
+            Console.WriteLine("la variable cadena tiene: {0}", cadena);
+            Console.WriteLine("la variable boolena tiene: {0}", valor);
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
+
+// esto e Variables ---->>>
+
+
+
+
+
+
+//esto es tipos de decimales ->>
+
+//int? i = null;
+//byte a = 0b1111_1111;
+//long k = 0xAFD12;
+//decimal d = 1 / 30000m;
+//float f = 1 / 30000f;
+//double g = 1 / 30000d;
+//bool bandera = true;
+//char c = 'a';
+//var s = 0;
+//Console.WriteLine("El valor de i es {0}", i);
+//Console.WriteLine("El valor de a es {0}", a);
+//Console.WriteLine("El valor de k es {0:x}", k);
+//Console.WriteLine("El valores con punto flotante");
+//Console.WriteLine("El valor de d es {0}", d);
+//Console.WriteLine("El valor de f es {0}", f);
+//Console.WriteLine("El valor de g es {0}", g);
+//Console.WriteLine(bandera);
+//Console.WriteLine(c);
+//Console.WriteLine("Valores tipo va");
+//Console.WriteLine("Valores de s es {0} de tipo {1}" , s, s.GetType());
+//Console.ReadKey();
+
+
+
+//string cadena = "hola mundo ";
+//string cadena2 = cadena;
+//cadena = "Hola programadores\n\"mensaje de bienvenida\"";
+//string ruta = /*@*/"C:\\Usuarios\\Programas\\Fernando";  // --->> se pone el arroba para especificar la ruta tal y como esta
+//string programa = "a";
+//Console.WriteLine(cadena);
+//Console.WriteLine(cadena2);
+//Console.WriteLine(ruta);
+//Console.WriteLine(programa);
+//Console.ReadKey();
